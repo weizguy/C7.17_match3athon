358ac7bcb052145c53bdf5205d290e38332cfd8c
weizguy <dave.weizenegger@gmail.com>
2017-08-09 22:59:09 -0700

    finished overall presentation

--- js/game.js
+++ js/game.js
@@ -1,10 +1,71 @@
 /**
  * Created by Weizguy on 8/9/2017.
  */
+var img = null;
+var row = 0;
+var column = 0;
+var score = 0;
+var need = 1000;
+var level = 0;
+
 $(document).ready(function() {
     createBoard();
     shuffleChars(chars);
-})
+    loadClickHandlers();
+});
+
+function openStartScreen() {
+    $('#startScreen').show();
+}
+
+$('#btn').on('click', function(){
+    shuffleChars(chars);
+    resetScore();
+});
+
+function scoreLevel(){
+    switch(level){
+        case 1:
+            need = 1000;
+            break;
+        case 2:
+            need = 2500;
+            break;
+        case 3:
+            need = 5000;
+            break;
+        case 4:
+            need = 7500;
+            break;
+        case 5:
+            need = 10000;
+            break;
+        case 6:
+            need = 15000;
+            break;
+        case 7:
+            need = 20000;
+            break;
+        case 8:
+            need = 30000;
+            break;
+        case 9:
+            need = 50000;
+            break;
+        case 10:
+            need = 100000;
+            break;
+    }
+}
+
+function resetScore(){
+    level = 1;
+    need = 1000;
+    score = 0;
+    $('#levelText').text(level);
+    $('#needText').text(need.toLocaleString());
+    $('#scoreText').text(score.toLocaleString());
+}
 
 function createBoard() {
     var $gameGrid = $('#gameGrid');
@@ -16,19 +77,30 @@ function createBoard() {
             $('.row'+i).append(square);
         }
     }
-    loadClickHandlers();
+   resetScore();
 }
+
 function loadClickHandlers(){
+    $('#play').click(play);
     $('.gameSquare').click(clicker);
 }
 
+function play() {
+    $('#startScreen').hide();
+}
+
 function clicker() {
-    var row = $(this).data('row') + 1;
-    var column = $(this).data('column') + 1;
-
-    console.log("row :" + row);
-    console.log("column :" + column);
-
+    row = $(this).data('row') + 1;
+    column = $(this).data('column') + 1;
+    img = $(this).context.style.backgroundImage;
+    score += 100;
+    $('#scoreText').text(score.toLocaleString());
+    if(score === need){
+        level += 1;
+        $('#levelText').text(level);
+        scoreLevel();
+        $('#needText').text(need.toLocaleString());
+    }
 }
 
 // function to assign the card faces START
@@ -56,79 +128,22 @@ function shuffleChars(array) {
 
 // create the array of characters START
 // ***********************************************************************************
-var chars = [
-    {   name: "Homer", img: "images/homer.jpg", id: 1  },
-    {   name: "Marge", img: "images/marge.png", id: 2 },
-    {   name: "Bart", img: "images/bart.jpg", id: 3 },
-    {   name: "Lisa", img: "images/lisa.png", id: 4 },
-    {   name: "Maggie", img: "images/maggie.png", id: 5 },
-    {   name: "Santas Helper", img: "images/santashelper.png", id: 6 },
-    {   name: "Krusty", img: "images/krusty.jpg", id: 7 },
-    {   name: "Itchy", img: "images/itchy.png", id: 8 },
-    {   name: "Scratchy", img: "images/scratchy.jpg", id: 9 },
-    {   name: "Chief Wiggums", img: "images/chief.jpg", id: 10 },
-    {   name: "Flanders", img: "images/flanders.png", id: 11 },
-    {   name: "Grandpa", img: "images/grandpa.png", id: 12 },
-    {   name: "Homer", img: "images/homer.jpg", id: 13  },
-    {   name: "Marge", img: "images/marge.png", id: 14 },
-    {   name: "Bart", img: "images/bart.jpg", id: 15 },
-    {   name: "Lisa", img: "images/lisa.png", id: 16 },
-    {   name: "Maggie", img: "images/maggie.png", id: 17 },
-    {   name: "Santas Helper", img: "images/santashelper.png", id: 18 },
-    {   name: "Krusty", img: "images/krusty.jpg", id: 19 },
-    {   name: "Itchy", img: "images/itchy.png", id: 20 },
-    {   name: "Scratchy", img: "images/scratchy.jpg", id: 21 },
-    {   name: "Chief Wiggums", img: "images/chief.jpg", id: 22 },
-    {   name: "Flanders", img: "images/flanders.png", id: 23 },
-    {   name: "Grandpa", img: "images/grandpa.png", id: 24 },
-    {   name: "Homer", img: "images/homer.jpg", id: 25  },
-    {   name: "Marge", img: "images/marge.png", id: 26 },
-    {   name: "Bart", img: "images/bart.jpg", id: 27 },
-    {   name: "Lisa", img: "images/lisa.png", id: 28 },
-    {   name: "Maggie", img: "images/maggie.png", id: 29 },
-    {   name: "Santas Helper", img: "images/santashelper.png", id: 30 },
-    {   name: "Krusty", img: "images/krusty.jpg", id: 31 },
-    {   name: "Itchy", img: "images/itchy.png", id: 32 },
-    {   name: "Scratchy", img: "images/scratchy.jpg", id: 33 },
-    {   name: "Chief Wiggums", img: "images/chief.jpg", id: 34 },
-    {   name: "Flanders", img: "images/flanders.png", id: 35 },
-    {   name: "Grandpa", img: "images/grandpa.png", id: 36 },
-    {   name: "Homer", img: "images/homer.jpg", id: 37  },
-    {   name: "Marge", img: "images/marge.png", id: 38 },
-    {   name: "Bart", img: "images/bart.jpg", id: 39 },
-    {   name: "Lisa", img: "images/lisa.png", id: 40 },
-    {   name: "Maggie", img: "images/maggie.png", id: 41 },
-    {   name: "Santas Helper", img: "images/santashelper.png", id: 42 },
-    {   name: "Krusty", img: "images/krusty.jpg", id: 43 },
-    {   name: "Itchy", img: "images/itchy.png", id: 44 },
-    {   name: "Scratchy", img: "images/scratchy.jpg", id: 45 },
-    {   name: "Chief Wiggums", img: "images/chief.jpg", id: 46 },
-    {   name: "Flanders", img: "images/flanders.png", id: 47 },
-    {   name: "Grandpa", img: "images/grandpa.png", id: 48 },
-    {   name: "Homer", img: "images/homer.jpg", id: 49  },
-    {   name: "Marge", img: "images/marge.png", id: 50 },
-    {   name: "Bart", img: "images/bart.jpg", id: 51 },
-    {   name: "Lisa", img: "images/lisa.png", id: 52 },
-    {   name: "Maggie", img: "images/maggie.png", id: 53 },
-    {   name: "Santas Helper", img: "images/santashelper.png", id: 54 },
-    {   name: "Krusty", img: "images/krusty.jpg", id: 55 },
-    {   name: "Itchy", img: "images/itchy.png", id: 56 },
-    {   name: "Scratchy", img: "images/scratchy.jpg", id: 57 },
-    {   name: "Chief Wiggums", img: "images/chief.jpg", id: 58 },
-    {   name: "Flanders", img: "images/flanders.png", id: 59 },
-    {   name: "Grandpa", img: "images/grandpa.png", id: 60 },
-    {   name: "Homer", img: "images/homer.jpg", id: 61  },
-    {   name: "Marge", img: "images/marge.png", id: 62 },
-    {   name: "Bart", img: "images/bart.jpg", id: 63 },
-    {   name: "Lisa", img: "images/lisa.png", id: 64 },
-    {   name: "Maggie", img: "images/maggie.png", id: 65 },
-    {   name: "Santas Helper", img: "images/santashelper.png", id: 66 },
-    {   name: "Krusty", img: "images/krusty.jpg", id: 67 },
-    {   name: "Itchy", img: "images/itchy.png", id: 68 },
-    {   name: "Scratchy", img: "images/scratchy.jpg", id: 69 },
-    {   name: "Chief Wiggums", img: "images/chief.jpg", id: 70 },
-    {   name: "Flanders", img: "images/flanders.png", id: 71 },
-    {   name: "Grandpa", img: "images/grandpa.png", id: 72 }
+var characters = [
+    {   name: "Homer", img: "images/homer.jpg"  },
+    {   name: "Marge", img: "images/marge.png" },
+    {   name: "Bart", img: "images/bart.jpg" },
+    {   name: "Lisa", img: "images/lisa.png" },
+    {   name: "Maggie", img: "images/maggie.png" },
+    {   name: "Santas Helper", img: "images/santashelper.png" },
+    {   name: "Krusty", img: "images/krusty.jpg" },
+    {   name: "Itchy", img: "images/itchy.png" },
+    {   name: "Scratchy", img: "images/scratchy.jpg" },
+    {   name: "Chief Wiggums", img: "images/chief.jpg" },
+    {   name: "Flanders", img: "images/flanders.png" },
+    {   name: "Grandpa", img: "images/grandpa.png" }
 ];
+var chars = Array.apply(null, {length: 6 * characters.length}).map(function(e,i){return characters[i % characters.length]});
 // ***********************************************************************************
 // create the array of characters END
+
+
